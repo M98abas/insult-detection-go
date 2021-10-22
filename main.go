2a9b57b637f42a46f20ef115b841a2fede0fa411
M98abas <mahmoud.ud@gmail.com>
2021-10-22 18:06:42 +0300

    final-conf

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+
 	routes "example.com/new-app/routes"
 	"github.com/gin-gonic/gin"
 )
  func main() {
 	 // Init Router
+	 gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	// Route Handlers / Endpoints
 	routes.Routes(router)
